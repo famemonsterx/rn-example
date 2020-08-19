1bd0d9584f4853fc1dec233b9ab77ff22c01b319
famemonsterx <zesikx@yandex.ru>
2020-08-19 17:49:11 +0300

    commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import AppNavigation from "./components/MainNavigation/AppNavigation";
+
+export default function App() {
+  return (
+      <AppNavigation/>
+  );
+}
+
+
